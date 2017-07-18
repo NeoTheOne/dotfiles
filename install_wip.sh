9ab65d468024bdfd2f820469d95b19ec34e5a26a
glandau <gabe.landau@endurance.com>
2017-07-18 10:57:24 -0400

    Updated install script

--- install_wip.sh
+++ install_wip.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 #================================
 # Run this script to install configuration files and symlink them to the home directory
-# Be sure to change $USERNAME to reflect the your username
 #================================
 
-# Variables
-USER=glandau
+### Create repos directory for antigen and other dependencies
+mkdir repos
 
-# Symlinks
-ln -s /Users/$USER/dotfiles/zsh/zshrc /Users/$USER/.zshrc
+### Download dependencies
+# Antigen
+git clone https://github.com/zsh-users/antigen.git $HOME/dotfiles/repos
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+# Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+### Rename/backup old/default dotfiles
+mv $HOME/.zshrc $HOME/.zshrc_old
+mv $HOME/.bash_profile $HOME/.bash_profile_old
+mv $HOME/.vimrc $HOME/.vimrc_old
+
+### Symlinks
+ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/bash/bash_profile $HOME/.bash_profile
+ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
