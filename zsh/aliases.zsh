08df242f567b86443795d97fc3ef8c3a173afd52
gabelandau <gabelandau14@gmail.com>
2017-08-31 00:45:43 -0400

    Cleaned up aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,10 +13,6 @@ alias repo='cd ~/repos'
 alias repos='cd ~/repos'
 alias dotfiles='cd ~/dotfiles'
 alias dev='cd ~/development'
-alias mamp='cd ~/mamp'
-
-alias openmamp='open ~/mamp'
-alias openserver='open /Users/gabelandau/Documents/RIT/Class\ Files/Current\ Term/Server\ Programming'
 
 # Commands
 alias c='clear'
@@ -31,12 +27,3 @@ alias gfu='git fetch upstream'
 alias gmum='git merge upstream/master'
 alias gpo='git push origin'
 alias gpom='git push origin master'
-
-# RVM
-alias rvu='rvm use'
-alias rvuj179='rvm use jruby-1.7.19'
-alias rvu241='rvm use 2.4.1'
-alias rvd='rvm default'
-
-# Heroku
-alias deploydist='git subtree push --prefix dist heroku master'
