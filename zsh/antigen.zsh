a71b1a98076f31ebf80555f92848136536e7c194
Charlie Skog <charlie.skog@gmail.com>
2017-09-26 19:34:12 +0200

    Made own config file for antigen, added OS detection and fixed theme.

---
+++ zsh/antigen.zsh
@@ -0,0 +1,34 @@
+### antigen config
+
+### Load antigen
+
+# Determine OS and launch antigen
+if [[ $(uname -a) = *Darwin* ]]; then
+  source /usr/local/opt/antigen/share/antigen/antigen.zsh
+elif [[ $(uname -a) = *Debian* ]]; then
+  source /usr/share/zsh-antigen/antigen.zsh
+elif [[ $(uname -a) = *Ubuntu* ]]; then
+  source /usr/share/zsh-antigen/antigen.zsh
+fi
+
+
+### Load libraries
+
+# Load oh-my-zsh library
+antigen use oh-my-zsh
+
+
+### Load bundles
+
+# Load Git bundle
+antigen bundle git
+
+# Load dirhistory bundle
+antigen bundle dirhistory
+
+# Load zsh-syntax-highlighting bundle
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+
+### Apply antigen settings
+antigen apply
--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,9 +3,42 @@ if [ $UID -eq 0 ]; then CARETCOLOR="red"; else CARETCOLOR="blue"; fi
 local return_code="%(?..%{$fg[red]%}%? ↵%{$reset_color%})"
 
 NEWLINE=$'\n'
-PROMPT='%{$reset_color%}%{${fg[green]}%}%3~ $(git_prompt_info)${NEWLINE}%{${fg_bold[$CARETCOLOR]}%}»%{${reset_color}%} '
+PROMPT='%{$fg_bold[green]%}%n@%M%{$reset_color%}:%{$fg_bold[red]%}%~%{$reset_color%}$(git_prompt_info)$ '
 
 RPS1="${return_code}"
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[yellow]%}‹"
 ZSH_THEME_GIT_PROMPT_SUFFIX="› %{$reset_color%}"
+
+setopt extended_glob
+
+function precmd {
+        if [ $? -eq 0 ]; then
+                RPROMPT="( ˃ ヮ˂)"
+        else
+                RPROMPT="(◞ ‸ ◟ㆀ)"
+        fi
+
+    title "zsh" "%m(%55<...<%~)"
+}
+
+function preexec {
+    title "$1" "%m(%35<...<%~)"
+}
+
+function title() {
+  a=${(V)1//\%/\%\%}
+
+  a=$(print -Pn "%40>...>$a" | tr -d "\n")
+
+  case $TERM in
+  screen)
+    print -Pn "\e]2;$a :: $2\a" # plain xterm title
+    print -Pn "\ek$a\e\\"      # screen title (in ^A")
+    print -Pn "\e_$2   \e\\"   # screen location
+    ;;
+  xterm*|rxvt)
+    print -Pn "\e]2;$a :: $2\a" # plain xterm title
+    ;;
+  esac
+}
