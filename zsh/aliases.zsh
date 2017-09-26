9b59cae6205e80e1f6ded00399f15241da649057
Charlie Skog <charlie.skog@gmail.com>
2017-09-26 16:38:09 +0200

    Changed comments and added alias for editing paths.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,17 +1,20 @@
-# Config files
+### Aliases for zsh
+
+# Edit config file aliases
 alias zshrc='nano ~/dotfiles/zsh/zshrc'
 alias aliases='nano ~/dotfiles/zsh/aliases.zsh'
+alias paths='nano ~/dotfiles/zsh/paths.zsh'
 alias tmuxconf='nano ~/dotfiles/tmux/tmux.conf'
 alias bashprofile='nano ~/dotfiles/bash/bash_profile'
 alias hosts='sudo nano /etc/hosts'
 
-# Commands
+# Command aliases
 alias c='clear'
 alias lla='ll -a'
 alias ls='ls -hl --color=auto'
 alias grep='grep --color=auto'
 
-# Git
+# Git aliases
 alias gfu='git fetch upstream'
 alias gmum='git merge upstream/master'
 alias gpo='git push origin'
