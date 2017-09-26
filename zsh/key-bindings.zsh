93dbd7f189afca35085984af34ff32ffbaaaae13
Charlie Skog <charlie.skog@gmail.com>
2017-09-26 19:34:30 +0200

    Fixed if syntaxes.

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -1,6 +1,6 @@
 ### Key-bindings for zsh
 
 # Correct the escape sequence of the delete key on OSx in iTerm2
-if [ '$TERM_PROGRAM == "iTerm.app"' ]; then
-bindkey "^[[3~" delete-char
+if [[ $TERM_PROGRAM ==  "iTerm.app" ]]; then
+  bindkey "^[[3~" delete-char
 fi
