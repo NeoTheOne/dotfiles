5d576c25810f5016ab63724dac7b1f65f8699c3c
Charlie Skog <charlie.skog@gmail.com>
2017-09-27 10:12:25 +0200

    Fixed comment in paths.zsh

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -3,7 +3,7 @@
 #================================
 
 ### Paths
-# Path for gnu binaries for OSx
+# Path for gnu binaries for OSx via brew
 if [[ -d $(brew --prefix coreutils)/libexec/gnubin ]]; then
   export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 fi
