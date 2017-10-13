8532b93237abcaa899af64b602d9408f910c677a
Charlie Skog <charlie.skog@gmail.com>
2017-10-13 14:49:29 +0200

    Updated RDP Script
    Not really ready but it's on its way.
    Need to figure out a way to kill the Microsoft Remote Desktop window without killing the RDP sesssion. Maybe using applescript?

--- commands/rdp.sh
+++ commands/rdp.sh
@@ -8,23 +8,27 @@ rdpTempRDP=$TMPDIR/t.rdp
 rdpRunCommand="/Applications/Microsoft Remote Desktop.app/Contents/MacOS/Microsoft Remote Desktop"
 rdpHost=${1}
 
-if [ "${2}" = "/console" ] ; then
-    rdpHost="${rdpHost} /console";
-    echo "rdpHost is: " ${rdpHost}
+if [ "${1}" = "" ]; then
+  echo "Please type a valid hostname/IP"
+  exit
 fi
 
-if [ "${2}" = "-console" ] ; then
-    rdpHost="${rdpHost} /console";
-    echo "rdpHost is: " ${rdpHost}
+if [ "${2}" = "/console" ]; then
+  rdpHost="${rdpHost} /console";
+  echo "rdpHost is: " ${rdpHost}
 fi
 
-if [ "${2}" = "-admin" ] ; then
-    rdpHost="${rdpHost} /console";
-    echo "rdpHost is: " ${rdpHost}
+if [ "${2}" = "-console" ]; then
+  rdpHost="${rdpHost} /console";
+  echo "rdpHost is: " ${rdpHost}
+fi
+
+if [ "${2}" = "-admin" ]; then
+  rdpHost="${rdpHost} /console";
+  echo "rdpHost is: " ${rdpHost}
 fi
 
 cp -f ${rdpTemplateFile} ${rdpTempRDP}
-
 sed -i "s|zzxyzyz|${rdpHost}|g" ${rdpTempRDP}
 
 open -na "${rdpRunCommand}" ${rdpTempRDP}
