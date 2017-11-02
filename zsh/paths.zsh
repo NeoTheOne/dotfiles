b4122bd27dda25b727cb36fdf5ada9588de9c19a
Charlie Skog <charlie.skog@gmail.com>
2017-11-02 10:19:26 +0100

    Added OS recognization for paths

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -3,7 +3,11 @@
 #==================
 
 ### Paths
-# Path for gnu binaries for OSx via brew
-if [[ -d $(brew --prefix coreutils)/libexec/gnubin ]]; then
-  export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+if [[ $(uname -a) = *Darwin* ]]; then
+  # Path for gnu binaries for OSx via brew
+  if [[ -d $(brew --prefix coreutils)/libexec/gnubin ]]; then
+    export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+  fi
+elif [[ $(uname -a) = *Debian* ]]; then
+elif [[ $(uname -a) = *Ubuntu* ]]; then
 fi
