55e531cd4c796ce7577baa0e0cc5db1c56862dd9
Charlie Skog <charlie.skog@gmail.com>
2017-11-02 11:20:08 +0100

    Added neofetch config

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #====================================================================================================
 # Run this script to download the repository, install dependencies and symlink the config files
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/NeoTheOne/dotfiles/master/install.sh)"
+# !!!CAUTION!!! The script restarts your computer so make any backups needed.
 #====================================================================================================
 
 ### Activate sudo
@@ -37,12 +38,43 @@ sudo chsh -s /bin/zsh $USER
 ### Allow command scripts to be run
 sudo chmod +x $HOME/dotfiles/commands/*
 
-### Rename/backup old/default dotfiles
-mv $HOME/.zshrc $HOME/.zshrc_old
-mv $HOME/.bash_profile $HOME/.bash_profile_old
-mv $HOME/.tmux.conf $HOME/.tmux.conf_old
+### Rename/backup old/default dotfiles and create directories for the new ones
+if [ -f $HOME/.zshrc ]; then
+  if [ ! -d $HOME/.old ]; then
+    mkdir $HOME/.old
+  fi
+  mv $HOME/.zshrc $HOME/.old/zshrc_old
+fi
+if [ -f $HOME/.bash_profile ]; then
+  if [ ! -d $HOME/.old ]; then
+    mkdir $HOME/.old
+  fi
+  mv $HOME/.bash_profile $HOME/.old/bash_profile_old
+fi
+if [ -f $HOME/.tmux.conf ]; then
+  if [ ! -d $HOME/.old ]; then
+    mkdir $HOME/.old
+  fi
+  mv $HOME/.tmux.conf $HOME/.old/tmux.conf_old
+fi
+if [ -f $HOME/.config/neofetch/config ]; then
+  if [ ! -d $HOME/.old ]; then
+    mkdir $HOME/.old
+  fi
+  mv $HOME/.config/neofetch/config $HOME/.old/neofetch_config_old
+fi
+if [ ! -d $HOME/.config/neofetch ]; then
+  if [ ! -d $HOME/.config ]; then
+    mkdir $HOME/.config
+  fi
+  mkdir $HOME/.config/neofetch
+fi
 
 ### Symlinks
 ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/bash/bash_profile $HOME/.bash_profile
 ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/neofetch/config $HOME/.config/neofetch/config
+
+### Restart to apply changes to wm terminals
+sudo shutdown -r now
