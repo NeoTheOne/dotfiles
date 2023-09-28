4fabeaddfb1a94fb9e334385f09458fa79376fe5
NeoTheOne <charlie.skog@gmail.com>
2023-09-28 10:58:06 +0200

    Update aliases.zsh
    dot the dotfiles folder :-)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,12 +4,12 @@
 
 ### Aliases
 # Config edit aliases
-alias zshrc='nano ~/dotfiles/zsh/zshrc'
-alias antigenconf='nano ~/dotfiles/zsh/antigen.zsh'
-alias aliases='nano ~/dotfiles/zsh/aliases.zsh'
-alias paths='nano ~/dotfiles/zsh/paths.zsh'
-alias tmuxconf='nano ~/dotfiles/tmux/tmux.conf'
-alias bashprofile='nano ~/dotfiles/bash/bash_profile'
+alias zshrc='nano ~/.dotfiles/zsh/zshrc'
+alias antigenconf='nano ~/.dotfiles/zsh/antigen.zsh'
+alias aliases='nano ~/.dotfiles/zsh/aliases.zsh'
+alias paths='nano ~/.dotfiles/zsh/paths.zsh'
+alias tmuxconf='nano ~/.dotfiles/tmux/tmux.conf'
+alias bashprofile='nano ~/.dotfiles/bash/bash_profile'
 alias hosts='sudo nano /etc/hosts'
 # Command aliases
 alias c='clear'
